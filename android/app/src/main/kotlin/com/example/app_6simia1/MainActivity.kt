c97dbddf20b08ce41d1d3234b9440c7d5ab82f70
Azlan-saja <azlansaja19@gmail.com>
2024-02-12 12:54:54 +0700

    first project

---
+++ android/app/src/main/kotlin/com/example/app_6simia1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_6simia1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text('Hey,'),
+          Text('Login Now!'),
+        ],
+      ),
+    );
+  }
+}
